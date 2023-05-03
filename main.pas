ba964ca748ed2c8b6a5d4cc84880282d74f3eb2d
Victor <128752738+TheWinterComes@users.noreply.github.com>
2023-05-03 20:55:42 -0300

    Update main.pas

--- main.pas
+++ main.pas
@@ -9,18 +9,23 @@ Write your code in this editor and press "Run" button to execute it.
 
 program ClassificarNadador;
 var
-idade: integer;
+v_idade: integer;
+v_categoria : string [15]
 begin
 write('Digite a idade do nadador: ');
-readln(idade);
-if (idade >= 5) and (idade <= 7) then
-writeln('Infantil A')
-else if (idade >= 8) and (idade <= 10) then
-writeln('Infantil B')
-else if (idade >= 11) and (idade <= 13) then
-writeln('Juvenil A')
-else if (idade >= 14) and (idade <= 17) then
-writeln('Juvenil B')
+readln(v_idade);
+   if (v_idade >= 5) and (v_idade <= 7) then
+           v_categoria:='Infantil A'
+else 
+   if (v_idade >= 8) and (v_idade <= 10) then
+          v_categoria:='Infantil B'
+else 
+   if (v_idade >= 11) and (v_idade <= 13) then
+          v_categoria:='Juvenil A'
+else 
+   if (v_idade >= 14) and (v_idade <= 17) then
+          v_categoria:='Juvenil B'
 else
-writeln('Sênior');
+          v_categoria := 'Sênior'
+writeln('A categoria do nadador é :', v_categoria);
 end.
